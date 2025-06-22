50ae21ffa4e458bca3dc460f9f02b96d6c286416
YuuinIH <37664092+YuuinIH@users.noreply.github.com>
2025-06-22 23:12:47 +0800

    feat: mark and emblem image

--- packages/web-ui/src/components/battle/Mark.vue
+++ packages/web-ui/src/components/battle/Mark.vue
@@ -6,11 +6,13 @@ import type { MarkMessage } from '@arcadia-eternity/const'
 import { useResourceStore } from '@/stores/resource'
 import { Z_INDEX } from '@/constants/zIndex'
 import i18next from 'i18next'
+import { useGameDataStore } from '@/stores/gameData'
 
 const md = new MarkdownIt({
   html: true,
 })
 const resourceStore = useResourceStore()
+const dataStore = useGameDataStore()
 
 const props = withDefaults(
   defineProps<{
@@ -22,9 +24,17 @@ const props = withDefaults(
 const rootEl = ref<HTMLElement | null>(null)
 const showTooltip = ref(false)
 const stackText = computed(() => `${props.mark.stack}`)
-const image = computed(
-  () => resourceStore.getMarkImage(props.mark.baseId) ?? 'https://seer2-resource.yuuinih.com/png/traitMark/inc.png',
-)
+const markData = computed(() => dataStore.getMark(props.mark.baseId))
+const image = computed(() => {
+  if (!markData.value) return 'https://seer2-resource.yuuinih.com/png/traitMark/inc.png'
+  if (markData.value.tags?.includes('ability')) {
+    return 'https://seer2-resource.yuuinih.com/png/markImage/ability.png'
+  }
+  if (markData.value.tags?.includes('emblem')) {
+    return 'https://seer2-resource.yuuinih.com/png/markImage/emblem.png'
+  }
+  return resourceStore.getMarkImage(props.mark.baseId) ?? 'https://seer2-resource.yuuinih.com/png/traitMark/inc.png'
+})
 const name = computed(() =>
   i18next.t(`${props.mark.baseId}.name`, {
     ns: ['mark', 'mark_ability', 'mark_emblem', 'mark_global'],
